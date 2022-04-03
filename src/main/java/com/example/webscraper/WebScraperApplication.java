0157cc6812b132c83255f688277e165f93bc0fd4
Baljeet Suri <Baljeet.x.Suri@cssregtech.com>
2022-04-04 00:12:28 +0530

    War packaging

--- src/main/java/com/example/webscraper/WebScraperApplication.java
+++ src/main/java/com/example/webscraper/WebScraperApplication.java
@@ -4,14 +4,14 @@ import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PreDestroy;
-import java.util.Arrays;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @CommonsLog
-public class WebScraperApplication {
+public class WebScraperApplication  {
 
 
     public static void main(String[] args) {
